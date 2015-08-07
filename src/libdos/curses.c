594359cb4e26408f1f6e7831027157aa96d166fe
Felix Palmen <felix@palmen-it.de>
2015-08-07 19:42:46 +0200

    curses: fix werase for subwins

--- src/libdos/curses.c
+++ src/libdos/curses.c
@@ -168,10 +168,11 @@ int werase(WINDOW *win)
     chtype ech = (win->bkgd & 0xff00) | 0x20;
     if (win->parent)
     {
-	for (int r = win->parent->row; r < win->parent->row + win->rows; ++r)
+	for (int r = win->row - win->parent->row;
+		r < win->row - win->parent->row + win->rows; ++r)
 	{
-	    for (int c = win->parent->col;
-		    c < win->parent->col + win->cols; ++c)
+	    for (int c = win->col - win->parent->col;
+		    c < win->col - win->parent->col + win->cols; ++c)
 	    {
 		win->parent->data[r*win->parent->cols + c] = ech;
 	    }
